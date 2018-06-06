1a60d1c9dbba87d1395c06b04ce5831ab11c9c48
zlig <gdevillard@gmail.com>
2018-06-06 23:02:59 +0100

    Initial project structure

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # twitter-github-mashup-api
+
+## Status
+
+[![Download](https://api.bintray.com/packages/geldtech/debian/twitter-github-mashup-api/images/download.svg)](https://bintray.com/geldtech/debian/twitter-github-mashup-api#files)
+[![Build Status](https://travis-ci.org/geld-tech/twitter-github-mashup-api.svg?branch=master)](https://travis-ci.org/geld-tech/twitter-github-mashup-api)
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+
+## Description
+
 Twitter/GitHub APIs Mashup Application built in Python Flask
+
+## Demo
+
+A sample demo of the project is hosted on <a href="http://geld.tech">geld.tech</a>.
+
+
+## Usage
+
+* Install the repository information and associated GPG key (to ensure authenticity):
+```
+$ echo "deb https://dl.bintray.com/geldtech/debian /" | sudo tee -a /etc/apt/sources.list.d/geld-tech.list
+$ sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com EA3E6BAEB37CF5E4
+```
+
+* Update repository list of available packages and clean already installed versions
+```
+$ sudo apt update
+$ sudo apt clean
+```
+
+* Install package
+```
+$ sudo apt install twitter-github-mashup-api
+```
+
+* Reload services and start ours
+```
+$ sudo systemctl daemon-reload
+$ sudo systemctl start twitter-github-mashup-api
+$ sudo systemctl status twitter-github-mashup-api
+```
+
+## Settings
+
+The following environment variables need to be configured in the Travis CI settings page to ensure a good build and deployment:
+
+```
+- BINTRAY_USER		Username used to upload to Bintray
+- BINTRAY_API_KEY	API Key used to upload in Bintray
+- BINTRAY_SUBJECT	User or organisation used to upload in Bintray
+- GA_UA_ID		Google Analytics User ID
+```
---
+++ sources/opt/geld/webapps/twitter-github-mashup-api/dependencies/py-mashup-api.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+"""
+	API mashup of GitHub and Twitter APIs
+    Search for "KEYWORD" projects on GitHub, then for each project search for tweets that mention it.
+	Outputs summary of each project with a short list of recent tweets (JSON format)
+
+	./py-mashup-api.py --consumerkey <CONSUMER_KEY> --consumersecret <CONSUMER_SECRET> --keyword <KEYWORD>
+	
+"""
+
+import base64
+import datetime
+import csv
+import json
+import logging
+import logging.handlers
+import os
+import platform
+import Queue
+import re
+import requests
+from requests.auth import HTTPBasicAuth
+import subprocess
+import sys
+import threading
+import time
+import urllib
+import urllib2
+from optparse import OptionParser
+import oauth2
+import io
+
+# Twitter Application Access Token and Secret
+twitter_access_token=""
+twitter_access_secret=""
+
+# Globals
+github_api_uri="https://api.github.com/search/repositories?q="
+github_projects_limit=10
+twitter_api_uri_timeline='https://api.twitter.com/1.1/statuses/home_timeline.json'
+twitter_api_uri_search='https://api.twitter.com/1.1/search/tweets.json'
+
+# Initialisation
+logging.basicConfig(format='[%(asctime)-15s] [%(threadName)s] %(levelname)s %(message)s',level=logging.INFO)
+logger = logging.getLogger('root')
+
+def oauth_req(url, key, secret, http_method="GET", post_body="", http_headers=None):
+    consumer = oauth2.Consumer(key=key, secret=secret)
+    token = oauth2.Token(key=twitter_access_token, secret=twitter_access_secret)
+    client = oauth2.Client(consumer, token)
+    resp, content = client.request( url, method=http_method, body=post_body, headers=http_headers )
+    return content
+
+# Functions
+def main():
+    tweets_list=[]
+    try:
+        # Parse options
+        parser = OptionParser()
+        parser.add_option('--consumerkey', action='store', dest='twitter_consumer_key', help='Twitter Consumer Key.')
+        parser.add_option('--consumersecret', action='store', dest='twitter_consumer_secret', help='Twitter Consumer Secret.')
+	parser.add_option('--keyword', action='store', dest='keyword', help='Keyword to search Github for and to identify in Twitter.')
+        parser.add_option('--debug', action='store_true', dest='debug', help='Print verbose output.', default=False)
+        options, args = parser.parse_args()
+        if not options.twitter_consumer_key:
+            parser.error('Twitter Consumer Key not provided, use -h to show the help')
+        if not options.twitter_consumer_secret:
+            parser.error('Twitter Consumer Secret not provided, use -h to show the help')
+        if not options.keyword:
+            parser.error('Keyword not provided, use -h to show the help')
+        if options.debug:
+            logger.setLevel(logging.DEBUG)
+            logger.debug('Enabled DEBUG logging level.')
+        logger.info('Options parsed')
+        
+	# GitHub Query
+	logger.info('Querying GitHub for %s' % options.keyword)
+	github_query_url = "%s%s" % (github_api_uri, options.keyword)
+	response = urllib.urlopen(github_query_url)
+	data = json.loads(response.read())
+	
+        # Twitter Client
+	logger.info('Search for tweets mentioning the GitHub projects identified by the keyword "%s"..' % options.keyword)
+	for item in data.get("items")[:github_projects_limit]:
+		twitter_search_tweets_url="%s?q=%s" % (twitter_api_uri_search, item.get("full_name"))
+		data = oauth_req(twitter_search_tweets_url, options.twitter_consumer_key, options.twitter_consumer_secret)
+		tweets = json.loads(data)
+		logger.info("Retrieved %d tweets mention the GitHub project %s" % (len(tweets['statuses']), item.get("full_name")))
+		for tweet in tweets['statuses']:
+			logger.debug(tweet)
+			tweets_list.append(tweet)
+	
+	logger.info('Retrieved %s tweets mentioned the projects identified by the keyword "%s"'% (len(tweets_list), options.keyword))
+	logger.debug('JSON contents dump: %s' % json.dumps(tweets_list, ensure_ascii=False))
+
+	filename="%s.json" % options.keyword
+	with io.open(filename, 'w', encoding='utf-8') as f:
+		f.write(json.dumps(tweets_list, ensure_ascii=False))
+
+	logger.info('Written result to file %s'% (filename))
+ 
+    except Exception, e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        logger.error('Exception %s in %s at line %d: %s' % (exc_type, fname, exc_tb.tb_lineno, e))
+
+# Main
+if __name__ == '__main__':
+    logger.info('Starting processing')
+    main()
+
