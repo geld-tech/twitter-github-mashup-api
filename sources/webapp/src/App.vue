723295989e1838a810dc4a9d07855d2d93e5101a
zlig <gdevillard@gmail.com>
2018-06-27 22:58:33 +0100

    Makes page content responsive

--- sources/webapp/src/App.vue
+++ sources/webapp/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
     <!-- Navigation -->
-    <b-navbar class="navbar-expand-lg navbar-light bg-white fixed-top">
-      <b-navbar-brand href="/">
-        <img src="/static/images/geld.tech_32x32.png" width="30" height="30" alt="" /> __PACKAGE_NAME__
-      </b-navbar-brand>
-      <b-navbar-nav class="ml-auto">
-        <b-nav-form @submit="onSubmit" @reset="onReset">
-          <b-form-input id="searchInput" type="search"  v-model="form.keyword" class="form-control mr-sm-2" placeholder="" aria-label="Search"></b-form-input>
-          <b-button class="my-2 my-sm-0" variant="primary" type="submit">Search</b-button>
-        </b-nav-form>
-      </b-navbar-nav>
+    <b-navbar class="navbar-expand-lg navbar-light bg-white fixed-top" toggleable>
+        <b-navbar-nav>
+          <b-navbar-brand href="/"><img src="/static/images/geld.tech_32x32.png" width="30" height="30" alt="" /> __PACKAGE_NAME__</b-navbar-brand>
+        </b-navbar-nav>
+        <b-navbar-nav class="ml-auto">
+            <b-nav-form @submit="onSubmit" @reset="onReset">
+              <b-form-input id="searchInput" type="search"  v-model="form.keyword" class="form-control mr-sm-2 ml-auto" placeholder="" aria-label="Search"></b-form-input>
+              <b-button class="my-2 my-sm-0" variant="primary" type="submit">Search</b-button>
+            </b-nav-form>
+        </b-navbar-nav>
     </b-navbar>
     <!-- Container -->
     <b-container class="bv-example-row">
@@ -84,10 +84,26 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  margin-top: 60px;
-  padding-top: 70px;
+  margin-top: 30px;
+  padding-top: 50px;
+}
+@media screen and (min-width: 601px) {
+  #app {
+    margin-top: 5px;
+    padding-top: 70px;
+    font-size: 24px;
+  }
+}
+@media screen and (max-width: 600px) {
+  #app {
+    margin-top: 15px;
+    padding-top: 170px;
+    font-size: 14px;
+  }
 }
 .loading {
-    align: center;
+    width: 50%;
+    margin: 0;
+    padding-left: 20px;
 }
 </style>
